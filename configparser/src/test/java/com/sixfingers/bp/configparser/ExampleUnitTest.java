a2af58a14bd9ab8ce7a6756ee3581ffbb7f5a981
chattapa <abcd123!>
2018-11-12 01:06:28 +0530

    build script cleaned, test resource added

--- configparser/src/test/java/com/sixfingers/bp/configparser/ExampleUnitTest.java
+++ configparser/src/test/java/com/sixfingers/bp/configparser/ExampleUnitTest.java
@@ -1,6 +1,16 @@
 package com.sixfingers.bp.configparser;
 
+import android.util.Log;
+
 import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +24,14 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+
+    @Test
+    public void test() throws IOException{
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream("sample.json");
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(inputStream));
+        assertNotNull("ersferf",inputStreamReader.readLine());
+    }
+
+
 }
