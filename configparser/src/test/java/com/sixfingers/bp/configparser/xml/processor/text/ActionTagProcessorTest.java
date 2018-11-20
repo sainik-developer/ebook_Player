1496b3c81067a527510af19109b51ccb37764769
sainik <schattapadhyay@yapstone.com>
2018-11-20 21:08:16 +0530

    started writing test cases

--- configparser/src/test/java/com/sixfingers/bp/configparser/xml/processor/text/ActionTagProcessorTest.java
+++ configparser/src/test/java/com/sixfingers/bp/configparser/xml/processor/text/ActionTagProcessorTest.java
@@ -8,6 +8,12 @@ public class ActionTagProcessorTest {
 
     private ActionTagProcessor actionTagProcessor;
 
+    private final String VALID_ACTION_TAG = "<ac audio-time=\"0.598\">Nearly</ac>";
+    private final String VALID_ACTION_WITH_SPACE = "<ac audio-time=\"0.598\"> Nearly  </ac>";
+    private final String INVALID_NO_AUDIO_TIME_VALUE_ACTION_TAG = "<ac audio-time=\"\">Nearly</ac>";
+    private final String INVALID_NO_AUDIO_TIME_ACTION_TAG = "<ac >Nearly</ac>";
+    private final String INVALID_TAG_CLOSE_ACTION_TAG = "<ac >Nearly/ac>";
+
 
     @Before
     public void prepare() {
@@ -16,10 +22,32 @@ public class ActionTagProcessorTest {
 
 
     @Test
-    public void testChildTags() {
+    public void testChildTagsAndName() {
         Assert.assertTrue(actionTagProcessor.childTags().length == 0);
         Assert.assertTrue(actionTagProcessor.name().equals("ac"));
     }
 
+    @Test
+    public void testReadValid() {
+
+    }
+
+    @Test
+    public void testReadWithSpaceValid() {
+
+    }
+
+    @Test
+    public void testReadInvalidWithNoAudioTimeValue() {
+
+    }
+
+    @Test
+    public void testReadInvalidWithNoAudioTimeTag(){
+
+    }
+//
+//    @Test
+//    public void testRead
 
 }
